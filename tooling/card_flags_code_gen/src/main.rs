d28b27f14df1b74bac58a03c9113b94615408f30
Ryan1729 <Ryan1729@gmail.com>
2018-11-30 08:17:15 -0700

    get all card_flags tests passing!

--- tooling/card_flags_code_gen/src/main.rs
+++ tooling/card_flags_code_gen/src/main.rs
@@ -33,7 +33,7 @@ fn main() {
         Ok(file) => file,
     };
 
-    const RANK_COUNT: usize = 13;
+    const RANK_COUNT: u8 = 13;
 
     let mut macros = String::new();
 
@@ -47,70 +47,147 @@ fn main() {
         ($($stuff:tt)*) => {p!(&format!($($stuff)*))};
     }
 
+    let sorted_extrema = get_sorted_extrema(RANK_COUNT);
+
     p!("// generated file!\n\n");
     p!("macro_rules! consecutive_ranks {");
 
-    macro_rules! four_times {
-        ($fmt_str:expr) => {
-            for start in 0..RANK_COUNT {
-                for end in (start + 1)..RANK_COUNT {
-                    let range = start..=end;
+    for (start, end) in sorted_extrema.iter() {
+        let range = start..=end;
 
-                    macro_rules! in_range {
-                        ($e:expr) => {
-                            if range.contains(&$e) {
-                                1
-                            } else {
-                                0
-                            }
-                        };
-                    }
-
-                    pf!(
-                        $fmt_str,
-                        start,
-                        end,
-                        in_range!(12),
-                        in_range!(11),
-                        in_range!(10),
-                        in_range!(9),
-                        in_range!(8),
-                        in_range!(7),
-                        in_range!(6),
-                        in_range!(5),
-                        in_range!(4),
-                        in_range!(3),
-                        in_range!(2),
-                        in_range!(1),
-                        in_range!(0),
-                    );
+        macro_rules! in_range {
+            ($e:expr) => {{
+                let x: u8 = $e;
+                if range.contains(&&x) {
+                    1
+                } else {
+                    0
                 }
-            }
-        };
-    }
+            }};
+        }
 
-    four_times!("({}-{} clubs) => {{ 0b{}_{}{}{}{}_{}{}{}{}_{}{}{}{} }};");
-    four_times!("({}-{} diamonds) => {{ 0b{}_{}{}{}{}_{}{}{}{}_{}{}{}{}__0_0000_0000_0000 }};");
-    four_times!("({}-{} hearts) => {{ 0b{}_{}{}{}{}_{}{}{}{}_{}{}{}{}__0_0000_0000_0000__0_0000_0000_0000 }};");
-    four_times!("({}-{} spades) => {{ 0b{}_{}{}{}{}_{}{}{}{}_{}{}{}{}__0_0000_0000_0000__0_0000_0000_0000__0_0000_0000_0000 }};");
-
-    p!("}\n\n");
-
-    for suit in ["clubs", "diamonds", "hearts", "spades"].iter() {
-        for start in 0..RANK_COUNT {
-            for end in (start + 1)..RANK_COUNT {
+        macro_rules! four_times {
+            ($fmt_str:expr) => {
                 pf!(
-                    "consecutive_ranks!({0}-{1} {2}) => consecutive_ranks_result!({0}=>{1}, {2}),\n",
+                    $fmt_str,
                     start,
                     end,
-                    suit
+                    in_range!(12),
+                    in_range!(11),
+                    in_range!(10),
+                    in_range!(9),
+                    in_range!(8),
+                    in_range!(7),
+                    in_range!(6),
+                    in_range!(5),
+                    in_range!(4),
+                    in_range!(3),
+                    in_range!(2),
+                    in_range!(1),
+                    in_range!(0),
                 );
-            }
+            };
         }
+
+        four_times!("({}-{} clubs) => {{ 0b{}_{}{}{}{}_{}{}{}{}_{}{}{}{} }};");
+        four_times!("({}-{} diamonds) => {{ 0b{}_{}{}{}{}_{}{}{}{}_{}{}{}{}__0_0000_0000_0000 }};");
+        four_times!("({}-{} hearts) => {{ 0b{}_{}{}{}{}_{}{}{}{}_{}{}{}{}__0_0000_0000_0000__0_0000_0000_0000 }};");
+        four_times!("({}-{} spades) => {{ 0b{}_{}{}{}{}_{}{}{}{}_{}{}{}{}__0_0000_0000_0000__0_0000_0000_0000__0_0000_0000_0000 }};");
+    }
+    p!("}\n\n");
+
+    for (start, end) in sorted_extrema.iter() {
+        for suit in ["clubs", "diamonds", "hearts", "spades"].iter() {
+            pf!("consecutive_ranks!({0}-{1} {2}),\n", start, end, suit);
+        }
+    }
+
+    p!("\n\n");
+
+    for (start, end) in sorted_extrema.iter() {
+        for suit in ["clubs", "diamonds", "hearts", "spades"].iter() {
+            pf!(
+                "consecutive_ranks!({0}-{1} {2}) => consecutive_ranks_result!({0}=>{1}, {2}),\n",
+                start,
+                end,
+                suit
+            );
+        }
+    }
+
+    const DECK_SIZE: u8 = 52;
+
+    p!("\n\n");
+    p!("macro_rules! card_pattern {");
+    for i in 0..DECK_SIZE {
+        pf!("({}) => {{ {:052b} }};", i, 1u64 << i);
+    }
+    p!("}");
+
+    p!("\n\n");
+    for i in 0..DECK_SIZE {
+        pf!("card_pattern!({}),\n", i);
     }
 
     match file.write_all(macros.as_bytes()) {
         Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
         Ok(_) => println!("successfully wrote to {}", display),
     }
+}
+
+fn get_sorted_extrema(span: u8) -> Vec<(u8, u8)> {
+    let mut output = Vec::new();
+
+    for end in 1..(span - 1) {
+        output.push((0, end));
+    }
+
+    for start in 1..span {
+        for end in (start + 1)..span {
+            output.push((start, end));
+        }
+    }
+
+    output.sort_by_key(|(start, end)| 255 - (end - start));
+
+    output
+}
+
+#[test]
+fn test_get_sorted_extrema_on_small_input() {
+    let actual_extrema = get_sorted_extrema(4);
+
+    let actual_flags: Vec<u8> = actual_extrema
+        .into_iter()
+        .map(|(start, end)| {
+            let range = start..=end;
+
+            macro_rules! in_range_shifted {
+                ($e:expr) => {
+                    if range.contains(&$e) {
+                        1u8 << $e
+                    } else {
+                        0u8
+                    }
+                };
+            }
+
+            in_range_shifted!(3)
+                | in_range_shifted!(2)
+                | in_range_shifted!(1)
+                | in_range_shifted!(0)
+        }).collect();
+
+    let expected_flags = vec![0b1110, 0b0111, 0b1100, 0b0110, 0b0011];
+
+    let actual: Vec<u8> = actual_flags
+        .into_iter()
+        .map(|f: u8| f.count_ones() as u8)
+        .collect();
+    let expected: Vec<u8> = expected_flags
+        .into_iter()
+        .map(|f: u8| f.count_ones() as u8)
+        .collect();
+
+    assert_eq!(expected, actual);
 }
