dda91c662466f25de7efa268dd4d366a6eb3e35c
Ryan1729 <Ryan1729@gmail.com>
2018-12-20 00:04:29 -0700

    make the visualization and realize that it's essentially a 6-clique

--- tooling/card_movement_graph/src/main.rs
+++ tooling/card_movement_graph/src/main.rs
@@ -1,4 +1,4 @@
-use game_state::in_game::{CardMovement, RelativePlayerSet, RelativeHand};
+use game_state::in_game::{CardMovement, RelativePlayerSet, RelativePlayer, RelativeHand};
 use common::{AllValues, CardSelection};
 
 fn all_values_with_selection(selection: CardSelection) -> Vec<CardMovement> {
@@ -28,10 +28,37 @@ fn all_values_with_selection(selection: CardSelection) -> Vec<CardMovement> {
 }
 
 fn main() {
+    let mut output = String::new();
+
+    output.push_str(
+        "0 same\n1 next\n2 across\n3 previous\n4 deck\n5 discard\n#\n"
+    );
+
     let selection = CardSelection::all_values()[0];
     let cms = all_values_with_selection(selection);
-    for cm in cms.iter() {
-        println!("{}", cm);
+    use std::fmt::Write;
+    fn to_index(hand: RelativeHand) -> u8 {
+        match hand {
+            RelativeHand::Player(p) => match p{
+                RelativePlayer::Same => 0,
+                RelativePlayer::Next => 1,
+                RelativePlayer::Across => 2,
+                RelativePlayer::Previous => 3,
+         },
+           RelativeHand::Deck => 4,
+           RelativeHand::Discard => 5,
+        }
     }
-    println!("{}", cms.len());
+
+    for cm in cms.iter() {
+        write!(
+            output,
+            "{} {} {:04b}\n",
+            to_index(cm.source),
+            to_index(cm.target),
+            unsafe{std::mem::transmute::<RelativePlayerSet, u8>(cm.affected)}
+        );
+    }
+
+    println!("{}", output);
 }
