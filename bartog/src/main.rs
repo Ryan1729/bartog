85ed51708492ac71cf9b270f7d58db7a3d51c426
Ryan1729 <Ryan1729@gmail.com>
2022-07-29 01:06:00 -0600

    get web-sys version working, via cargo-run-wasm

---
+++ bartog/src/main.rs
@@ -0,0 +1,25 @@
+use web;
+
+use game;
+
+fn main() {
+    real_main();
+}
+
+#[cfg(feature = "wasm32")]
+mod wasm {
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen(start)]
+    pub fn run() {
+        //console_log::init_with_level(log::Level::Debug).expect("error initializing logger");
+
+        super::real_main();
+    }
+}
+
+fn real_main() {
+    let params = web::get_state_params();
+    let state = game::BartogState::new(params);
+    web::run(state);
+}
