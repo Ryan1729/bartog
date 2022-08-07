29e03c59c6df6b2c98b2ed80f499395f6b9381b4
Ryan1729 <Ryan1729@gmail.com>
2022-08-07 01:06:00 -0600

    remove log list

--- libs/web/src/web.rs
+++ libs/web/src/web.rs
@@ -21,18 +21,13 @@ pub fn run<S: State + 'static>(mut state: S) {
         .build(&event_loop)
         .unwrap();
 
-    #[cfg(target_arch = "wasm32")]
-    let log_list = wasm::create_log_list(&window);
-
+    // This must happen after the `build` call, or the stle gets overridden.
     #[cfg(target_arch = "wasm32")]
     wasm::style_canvas();
 
     let mut graphics_context = unsafe { GraphicsContext::new(window) }.unwrap();
 
     event_loop.run(move |event, _, control_flow| {
-        #[cfg(target_arch = "wasm32")]
-        wasm::log_event(&log_list, &event);
-
         let window = graphics_context.window();
 
         match event {
@@ -146,37 +141,6 @@ mod wasm {
             .dyn_into::<web_sys::HtmlCanvasElement>()
             .map_err(|_| ())
             .unwrap()
-    }
-
-    pub fn create_log_list(window: &Window) -> web_sys::Element {
-        let window = web_sys::window().unwrap();
-        let document = window.document().unwrap();
-        let body = document.body().unwrap();
-
-        let log_header = document.create_element("h2").unwrap();
-        log_header.set_text_content(Some("Event Log"));
-        body.append_child(&log_header).unwrap();
-
-        let log_list = document.create_element("ul").unwrap();
-        body.append_child(&log_list).unwrap();
-        log_list
-    }
-
-    pub fn log_event(log_list: &web_sys::Element, event: &Event<()>) {
-        //log::debug!("{:?}", event);
-
-        // Getting access to browser logs requires a lot of setup on mobile devices.
-        // So we implement this basic logging system into the page to give developers an easy alternative.
-        // As a bonus its also kind of handy on desktop.
-        if let Event::WindowEvent { event, .. } = &event {
-            let window = web_sys::window().unwrap();
-            let document = window.document().unwrap();
-            let log = document.create_element("li").unwrap();
-            log.set_text_content(Some(&format!("{:?}", event)));
-            log_list
-                .insert_before(&log, log_list.first_child().as_ref())
-                .unwrap();
-        }
     }
 }
 
