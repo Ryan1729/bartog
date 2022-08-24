6011ed79690628ecf966c29a9b96fd9d1525064c
Ryan1729 <Ryan1729@gmail.com>
2022-08-24 01:06:00 -0600

    add wrapper struct for state, in example

--- bartog/examples/investigation.rs
+++ bartog/examples/investigation.rs
@@ -1,5 +1,33 @@
+use platform_types::{Button, Command, SFX, State, StateParams};
+
+struct StateWrapper {
+    state: game::BartogState,
+}
+
+impl State for StateWrapper {
+    fn frame(&mut self) -> (&[Command], &[SFX]) {
+        self.state.frame()
+    }
+
+    fn press(&mut self, button: Button) {
+        self.state.press(button)
+    }
+
+    fn release(&mut self, button: Button) {
+        self.state.release(button)
+    }
+}
+
+impl StateWrapper {
+    fn new(params: StateParams) -> Self {
+        Self {
+            state: game::BartogState::new(params),
+        }
+    }
+}
+
 fn main() {
     let params = platform::get_state_params();
-    let state = game::BartogState::new(params);
+    let state = StateWrapper::new(params);
     platform::run(state);
 }
